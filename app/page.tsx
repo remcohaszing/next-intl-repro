a66095906b49ef7c01dcc5e2a800a46fdc0bbb31
Remco Haszing <remcohaszing@gmail.com>
2025-12-01 13:47:59 +0100

    In this code useExtracted is not replaced

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { Metadata } from 'next'
 import { useExtracted } from 'next-intl'
+import { getExtracted } from 'next-intl/server'
 
-export default function Page() {
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getExtracted()
+  
+  return {
+    title: t('Hello title!')
+  }
+}
+
+// The commented alternative works fine.
+// export default async function Page() {
+//   const t = await getExtracted()
+export default async function Page() {
   const t = useExtracted()
 
-  return t('Hello intl!')
+  return (
+    <div>
+      {t('Hello intl!')}
+    </div>
+  )
 }
